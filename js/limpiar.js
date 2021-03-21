a61e692ada237d6b64a497f019807248e696802b
Formulario43M <81119802+Formulario43M@users.noreply.github.com>
2021-03-21 17:50:54 -0600

    Delete js directory

--- js/limpiar.js
+++
@@ -1,8 +0,0 @@
-function limpia() {
-    salidaSuma.info(""); 
-    salidaResta.info(""); 
-    salidaMultiplica.info(""); 
-    salidaDividir.info("");
-    forma["num1"].value="";
-    forma["num2"].value="";
-}
