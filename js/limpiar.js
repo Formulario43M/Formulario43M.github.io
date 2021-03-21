86413ea7c408c011163ede81d6e2fd9127036dc8
Formulario43M <81119802+Formulario43M@users.noreply.github.com>
2021-03-21 17:15:05 -0600

    Create limpiar.js

---
+++ js/limpiar.js
@@ -0,0 +1,8 @@
+function limpia() {
+    salidaSuma.info(""); 
+    salidaResta.info(""); 
+    salidaMultiplica.info(""); 
+    salidaDividir.info("");
+    forma["num1"].value="";
+    forma["num2"].value="";
+}
